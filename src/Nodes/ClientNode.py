2615f242785e1e65b688d74ca9c5da47b375b8eb
bekj <bekj@itu.dk>
2022-04-25 12:58:15 +0200

    Nu gider jeg ikke mere slave arbejde

--- src/Nodes/ClientNode.py
+++ src/Nodes/ClientNode.py
@@ -20,6 +20,10 @@ class ClientNode (FastNode):
         self.debugPrint = False
         self.bid = bid
 
+        self.p = 0
+        self.h = Point(0, 0, self.pd.cp, check=False)
+        self.g = Point(0, 0, self.pd.cp, check=False)
+
         self.sk = SigningKey.generate()
         self.vk = self.sk.verifying_key
 
@@ -112,17 +116,18 @@ class ClientNode (FastNode):
         # secret_key =
 
         self.contractparams = get_message(self.bc_node).split(";")
-        p = int(self.contractparams[6])
-        g = str_to_point(self.contractparams[4], self.pd.cp)
+        self.p = int(self.contractparams[6])
+        self.h = str_to_point(self.contractparams[5], self.pd.cp)
+        self.g = str_to_point(self.contractparams[4], self.pd.cp)
 
         # Stage 2: Compute all big X's and send commits along with X to other nodes. Is used for stage three in veto
         commit_x_arr = []
         for i in range(len(self.bit_commitments)):
-            x = number.getRandomRange(1, p - 1)
+            x = number.getRandomRange(1, self.p - 1)
             self.small_xs.append(x)
 
             commit_and_big_x = self.bit_commitments[i][0].__str__(
-            ) + "|" + self.pd.cp.mul_point(x, g).__str__()
+            ) + "|" + self.pd.cp.mul_point(x, self.g).__str__()
 
             commit_and_big_x = commit_and_big_x + ";"
             commit_x_arr.append(commit_and_big_x)
@@ -175,12 +180,249 @@ class ClientNode (FastNode):
                 f"Failed when creating Y's for {self.id} - Big X's: {len(self.big_xs)}")
         # verify c_j for each other party p_j, skipped atm
 
+    def generate_bfv_nizk(self, i, c, v, X, Y, r, x, r_hat):
+        # Theoretically these could be the same, but we'll run that chance chief
+        if i == 1:
+            alpha = 2
+        else:
+            alpha = 1
+
+        v_s = self.sample_from_field_arr(4)
+
+        w_1, w_2 = 0, 0
+
+        if alpha == 1:  # F_1
+            w_2 = self.sample_from_field()
+        else:  # F_2
+            w_1 = self.sample_from_field()
+
+        c_div_g = self.pd.cp.sub_point(c, self.g)
+
+        # c^w_1 * h^v_1
+        t_1 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_1, c), self.pd.cp.mul_point(v_s[0], self.h))
+        t_1_p = Point(t_1.x % self.p, t_1.y % self.p, self.pd.cp)
+
+        # v^w_1 * Y^v_2
+        t_2 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_1, v), self.pd.cp.mul_point(v_s[1], Y))
+        t_2_p = Point(t_2.x % self.p, t_2.y % self.p, self.pd.cp)
+
+        # X^_w_1 * g^v_2
+        t_3 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_1, X), self.pd.cp.mul_point(v_s[1], self.g))
+        t_3_p = Point(t_3.x % self.p, t_3.y % self.p, self.pd.cp)
+
+        # (c/g)^w_2 * h^v_3
+        t_4 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_2, c_div_g), self.pd.cp.mul_point(v_s[2], self.h))
+        t_4_p = Point(t_4.x % self.p, t_4.y % self.p, self.pd.cp)
+
+        # v^w_2 * g^v_4
+        t_5 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_2, v), self.pd.cp.mul_point(v_s[3], self.g))
+        t_5_p = Point(t_5.x % self.p, t_5.y % self.p, self.pd.cp)
+
+        h_p = Point(self.h.x % self.p, self.h.y % self.p, self.pd.cp)
+        c_p = Point(c.x % self.p, c.y % self.p, self.pd.cp)
+        Y_p = Point(Y.x % self.p, Y.y % self.p, self.pd.cp)
+        v_p = Point(v.x % self.p, v.y % self.p, self.pd.cp)
+        g_p = Point(self.g.x % self.p, self.g.y % self.p, self.pd.cp)
+        X_p = Point(X.x % self.p, X.y % self.p, self.pd.cp)
+        c_div_g_p = Point(c_div_g.x % self.p, c_div_g.y % self.p, self.pd.cp)
+
+        h_add_arr = [self.h, c, Y, v, self.g,
+                     X, c_div_g, t_1, t_2, t_3, t_4, t_5]
+
+        h_p_add_arr = [h_p, c_p, Y_p, v_p, g_p, X_p,
+                       c_div_g_p, t_1_p, t_2_p, t_3_p, t_4_p, t_5_p]
+
+        if alpha == 1:  # F_1
+            gamma_1 = None  # H - (w_1 + w_2) (mod p)
+            gamma_2 = w_2
+
+            x_1, x_2, x_3, x_4 = r, x, 0, 0
+
+            # (v_1 - gamma_1 * x_1) mod p
+            r_1 = (v_s[0] - (gamma_1 * x_1)) % self.p
+
+            # (v_2 - gamma_1 * x_2) mod p
+            r_2 = (v_s[1] - (gamma_1 * x_2)) % self.p
+
+            # (v_2 - gamma_1 * x_2) mod p
+            r_3 = r_2
+
+            # (v_3 - gamma_1 * x_3) mod p
+            r_4 = (v_s[2] - (gamma_1 * x_3)) % self.p
+
+            # (v_4 - gamma_1 * x_4) mod p
+            r_5 = (v_s[3] - (gamma_1 * x_4)) % self.p
+
+        else:  # F_2
+            gamma_1 = w_1
+            gamma_2 = None  # H - (w_1 + w_2) (mod p)
+
+            x_1, x_2, x_3, x_4 = 0, 0, r, r_hat
+
+            # (v_1 - gamma_2 * x_1) mod p
+            r_1 = (v_s[0] - (gamma_2 * x_1)) % self.p
+
+            # (v_2 - gamma_2 * x_2) mod p
+            r_2 = (v_s[1] - (gamma_2 * x_2)) % self.p
+
+            # (v_2 - gamma_2 * x_2) mod p
+            r_3 = r_2
+
+            # (v_3 - gamma_2 * x_3) mod p
+            r_4 = (v_s[2] - (gamma_2 * x_3)) % self.p
+
+            # (v_4 - gamma_2 * x_4) mod p
+            r_5 = (v_s[3] - (gamma_2 * x_4)) % self.p
+
+        return (gamma_1, gamma_2, r_1, r_2, r_3, r_4, r_5)
+
+    def generate_afv_nizk(self, i, d, c, v, X, Y, Xr, Yr):
+        if i == 1 and d == 1:
+            alpha = 2
+        elif i == 1 and d == 0:
+            alpha = 3
+        else:
+            alpha = 1
+
+        v_s = self.sample_from_field_arr(8)
+
+        w_1, w_2, w_3 = 0, 0, 0
+
+        if i == 1 and d == 1:  # F_2
+            w_1, w_3 = self.sample_from_field(), self.sample_from_field()
+        elif i == i and d == 0:  # F_3
+            w_1, w_2 = self.sample_from_field(), self.sample_from_field()
+        else:  # F_1
+            w_2, w_3 = self.sample_from_field(), self.sample_from_field()
+
+        c_div_g = self.pd.cp.sub_point(c, self.g)
+
+        # c^w_1 * h^v_1
+        t_1 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_1, c), self.pd.cp.mul_point(v_s[0], self.h))
+        t_1_p = Point(t_1.x % self.p, t_1.y % self.p, self.pd.cp)
+
+        # v^w_1 * Y^v_2
+        t_2 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_1, v), self.pd.cp.mul_point(v_s[1], Y))
+        t_2_p = Point(t_2.x % self.p, t_2.y % self.p, self.pd.cp)
+
+        # X^_w_1 * g^v_2
+        t_3 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_1, X), self.pd.cp.mul_point(v_s[1], self.g))
+        t_3_p = Point(t_3.x % self.p, t_3.y % self.p, self.pd.cp)
+
+        # (c/g)^w_2 * h^v_3
+        t_4 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_2, c_div_g), self.pd.cp.mul_point(v_s[2], self.h))
+        t_4_p = Point(t_4.x % self.p, t_4.y % self.p, self.pd.cp)
+
+        # d^w_2 * g^v_4
+        t_5 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_2, d), self.pd.cp.mul_point(v_s[3], self.g))
+        t_5_p = Point(t_5.x % self.p, t_5.y % self.p, self.pd.cp)
+
+        # v^w_2 * g^v_5
+        t_6 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_2, v), self.pd.cp.mul_point(v_s[4], self.g))
+        t_6_p = Point(t_6.x % self.p, t_6.y % self.p, self.pd.cp)
+
+        # (c/g)^w_3 * h^v_6
+        t_7 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_3, c_div_g), self.pd.cp.mul_point(v_s[5], self.h))
+        t_7_p = Point(t_7.x % self.p, t_7.y % self.p, self.pd.cp)
+
+        # d^w_3 * Yr^v_7
+        t_8 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_3, d), self.pd.cp.mul_point(v_s[6], Yr))
+        t_8_p = Point(t_8.x % self.p, t_8.y % self.p, self.pd.cp)
+
+        # Xr^w_3 * g^v_7
+        t_9 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_3, Xr), self.pd.cp.mul_point(v_s[6], self.g))
+        t_9_p = Point(t_9.x % self.p, t_9.y % self.p, self.pd.cp)
+
+        # v^w_3 * Y^v_8
+        t_10 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_3, v), self.pd.cp.mul_point(v_s[7], Y))
+        t_10_p = Point(t_10.x % self.p, t_10.y % self.p, self.pd.cp)
+
+        # X^w_3 * g^v_8
+        t_11 = self.pd.cp.add_point(self.pd.cp.mul_point(
+            w_3, X), self.pd.cp.mul_point(v_s[7], self.g))
+        t_11_p = Point(t_11.x % self.p, t_11.y % self.p, self.pd.cp)
+
+        h_add_arr = [self.h, c, Y, v, self.g, X, c_div_g, d, Yr, Xr,
+                     t_1, t_2, t_3, t_4, t_5, t_6, t_7, t_8, t_9, t_10, t_11]
+
+        if alpha == 1:  # F_1
+            gamma_1 = None  # H - (w_1 + w_2 + w_3) (mod p)
+            gamma_2 = w_2
+            gamma_3 = w_3
+
+            r_1 = None
+            r_2 = None
+            r_3 = None
+            r_4 = None
+            r_5 = None
+            r_6 = None
+            r_7 = None
+            r_8 = None
+            r_9 = None
+            r_10 = None
+            r_11 = None
+        elif alpha == 2:  # F_2
+            gamma_1 = w_1
+            gamma_2 = None  # H - (w_1 + w_2 + w_3) (mod p)
+            gamma_3 = w_3
+
+            r_1 = None
+            r_2 = None
+            r_3 = None
+            r_4 = None
+            r_5 = None
+            r_6 = None
+            r_7 = None
+            r_8 = None
+            r_9 = None
+            r_10 = None
+            r_11 = None
+        else:  # F_3
+            gamma_1 = w_1
+            gamma_2 = w_2
+            gamma_3 = None  # H - (w_1 + w_2 + w_3) (mod p)
+
+            r_1 = None
+            r_2 = None
+            r_3 = None
+            r_4 = None
+            r_5 = None
+            r_6 = None
+            r_7 = None
+            r_8 = None
+            r_9 = None
+            r_10 = None
+            r_11 = None
+
+        return (gamma_1, gamma_2, gamma_3, r_1, r_2, r_3, r_4, r_5, r_6, r_7, r_8, r_9, r_10, r_11)
+
+    def sample_from_field(self):
+        return number.getRandomRange(1, self.p - 1)
+
+    def sample_from_field_arr(self, amount):
+        lst = []
+        for i in range(amount):
+            lst.append(self.sample_from_field())
+
+        return lst
+
     def veto(self):
         # Create NIZK :)
-
-        p = int(self.contractparams[6])
-        g = str_to_point(self.contractparams[4], self.pd.cp)
-
         bfv = True  # Before First Veto
 
         previous_vetos = []
@@ -189,89 +431,92 @@ class ClientNode (FastNode):
         print(
             f"{self.id} small_xs: {len(self.small_xs)}, big_ys: {len(self.big_ys[self.index])}")
 
-        try:
-            for i in range(len(self.bit_commitments)):
-                if bfv:  # Before first veto
-                    if self.bits[i] == 1:
-                        r = number.getRandomRange(1, p - 1)
-                        v = self.pd.cp.mul_point(r, g)
-                        previous_vetos.append(True)
-                    else:
-                        v = self.pd.cp.mul_point(
-                            self.small_xs[i], self.big_ys[self.index][i])
-                        previous_vetos.append(False)
+        for i in range(len(self.bit_commitments)):
+            if bfv:  # Before first veto
+                r_hat = number.getRandomRange(1, self.p - 1)
 
-                    self.send_to_nodes(str(v), exclude=[self.bc_node])
+                if self.bits[i] == 1:
+                    v = self.pd.cp.mul_point(r, self.g)
+                    previous_vetos.append(True)
+                else:
+                    v = self.pd.cp.mul_point(
+                        self.small_xs[i], self.big_ys[self.index][i])
+                    previous_vetos.append(False)
 
-                    time.sleep(0.1)
+                bfv_nizk = self.generate_bfv_nizk(
+                    self.bits[i], self.bit_commitments[i][0], v, self.big_ys[self.index][i], self.big_xs[self.index][i], self.bit_commitments[i][1], self.small_xs[i], r_hat)
 
-                    vs = get_all_messages_arr(self, len(self.clients))
+                self.send_to_nodes(str(v), exclude=[self.bc_node])
 
-                    v_arr = []
+                time.sleep(0.01)  # Can be adjusted to 0.01 for improved speed
 
-                    v_arr.append(v)
-                    for j in range(len(self.clients)):
-                        v_arr.append(str_to_point(vs[j], self.pd.cp))
+                vs = get_all_messages_arr(self, len(self.clients))
 
-                    point = g
+                v_arr = []
 
-                    for j in range(len(v_arr)):
-                        point = self.pd.cp.add_point(point, v_arr[j])
+                v_arr.append(v)
+                for j in range(len(self.clients)):
+                    v_arr.append(str_to_point(vs[j], self.pd.cp))
 
-                    if point != g:
-                        bfv = False
-                        latest_veto_r = i
-                        self.vetos.append(1)
-                    else:
-                        self.vetos.append(0)
+                point = self.g
 
-                    print(i)
+                for j in range(len(v_arr)):
+                    point = self.pd.cp.add_point(point, v_arr[j])
 
-                else:  # After first veto
-                    # If the bit is 1 and the previous veto was true
-                    if self.bits[i] == 1 and previous_vetos[latest_veto_r] == True:
-                        r = number.getRandomRange(1, p - 1)
-                        v = self.pd.cp.mul_point(r, g)
-                        previous_vetos.append(True)
-                    # If the bit is 1 and the previous veto was false
-                    elif self.bits[i] == 1 and previous_vetos[latest_veto_r] == False:
-                        v = self.pd.cp.mul_point(
-                            self.small_xs[i], self.big_ys[self.index][i])
-                        previous_vetos.append(False)
-                    else:  # If the bit is 0
-                        v = self.pd.cp.mul_point(
-                            self.small_xs[i], self.big_ys[self.index][i])
-                        previous_vetos.append(False)
+                if point != self.g:
+                    bfv = False
+                    latest_veto_r = i
+                    self.vetos.append(1)
+                else:
+                    self.vetos.append(0)
 
-                    self.send_to_nodes(str(v), exclude=[self.bc_node])
+                print(i)
 
-                    time.sleep(0.1)
+            else:  # After first veto
+                # If the bit is 1 and the previous veto was true
+                if self.bits[i] == 1 and previous_vetos[latest_veto_r] == True:
+                    r = number.getRandomRange(1, self.p - 1)
+                    v = self.pd.cp.mul_point(r, self.g)
+                    previous_vetos.append(True)
+                # If the bit is 1 and the previous veto was false
+                elif self.bits[i] == 1 and previous_vetos[latest_veto_r] == False:
+                    v = self.pd.cp.mul_point(
+                        self.small_xs[i], self.big_ys[self.index][i])
+                    previous_vetos.append(False)
+                else:  # If the bit is 0
+                    v = self.pd.cp.mul_point(
+                        self.small_xs[i], self.big_ys[self.index][i])
+                    previous_vetos.append(False)
 
-                    vs = get_all_messages_arr(self, len(self.clients))
+                afv_nizk = self.generate_afv_nizk(
+                    self.bits[i], self.bits[latest_veto_r], self.bit_commitments[i][0], v, self.big_ys[self.index][i], self.big_xs[self.index][i], self.big_ys[self.index][latest_veto_r], self.big_xs[self.index][latest_veto_r])
 
-                    v_arr = []
+                self.send_to_nodes(str(v), exclude=[self.bc_node])
 
-                    v_arr.append(v)
-                    for j in range(len(self.clients)):
-                        v_arr.append(str_to_point(vs[j], self.pd.cp))
+                time.sleep(0.01)  # Can be adjusted to 0.01 for improved speed
 
-                    point = g
+                vs = get_all_messages_arr(self, len(self.clients))
 
-                    for j in range(len(v_arr)):
-                        point = self.pd.cp.add_point(point, v_arr[j])
+                v_arr = []
 
-                    if point != g:
-                        latest_veto_r = i
-                        self.vetos.append(1)
-                    else:
-                        self.vetos.append(0)
+                v_arr.append(v)
+                for j in range(len(self.clients)):
+                    v_arr.append(str_to_point(vs[j], self.pd.cp))
 
-                    print(i)
+                point = self.g
 
-                time.sleep(0.1)
-        except Exception as e:
-            print(
-                f"FAILED: {self.id} small_xs: {len(self.small_xs)}, big_ys: {len(self.big_ys[self.index])}")
+                for j in range(len(v_arr)):
+                    point = self.pd.cp.add_point(point, v_arr[j])
+
+                if point != self.g:
+                    latest_veto_r = i
+                    self.vetos.append(1)
+                else:
+                    self.vetos.append(0)
+
+                print(i)
+
+            time.sleep(0.1)
 
     def veto_output(self):
         self.send_to_nodes(str(self.vetos), exclude=[self.bc_node])
@@ -313,14 +558,6 @@ class ClientNode (FastNode):
         accept_connections_thread.start()
 
         self.connect_to_nodes()
-
-        # self.send_to_nodes(({"input": "setup", "input2": self.clients[0]}))
-
-        # msg = get_all_messages_arr(self, len(self.clients))
-
-        # dict1 = json.loads(msg[0])
-
-        # print(dict1["input"])
 
         self.setup()
 
